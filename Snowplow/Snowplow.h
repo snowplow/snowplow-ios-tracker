5ac03d47622b14e730a467e656ecf31c2d2be215
Jonathan Almeida <jonathan.almeida942@gmail.com>
2014-05-08 00:33:08 -0400

    Initial Commit

---
+++ Snowplow/Snowplow.h
@@ -0,0 +1,13 @@
+//
+//  Snowplow.h
+//  Snowplow
+//
+//  Created by Jonathan Almeida on 2014-05-08.
+//  Copyright (c) 2014 Snowplow Analytics. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Snowplow : NSObject
+
+@end
---
+++ Snowplow/Snowplow.m
@@ -0,0 +1,13 @@
+//
+//  Snowplow.m
+//  Snowplow
+//
+//  Created by Jonathan Almeida on 2014-05-08.
+//  Copyright (c) 2014 Snowplow Analytics. All rights reserved.
+//
+
+#import "Snowplow.h"
+
+@implementation Snowplow
+
+@end
