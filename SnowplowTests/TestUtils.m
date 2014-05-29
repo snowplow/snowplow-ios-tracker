bb3043cf0253e9da3f078b59d75bb2047e34be74
Jonathan Almeida <jonathan@snowplowanalytics.com>
2014-05-29 18:14:22 -0400

    Removed OpenIDFA test case

--- SnowplowTests/TestUtils.m
+++ SnowplowTests/TestUtils.m
@@ -99,23 +99,6 @@
                    @"UUID generated doesn't match the type 4 UUID RFC");
 }
 
-- (void)testGetOpenIdfa
-{
-    NSString *sample_uuid = [SnowplowUtils getOpenIdfa];
-    
-    // For regex pattern matching to verify if it's of UUID type 4
-    NSString *pattern = @"[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}";
-    NSRange searchRange = NSMakeRange(0, [sample_uuid length]);
-    NSError *error = NULL;
-    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:pattern options:0 error:&error];
-    NSArray *matches = [regex matchesInString:sample_uuid options:0 range:searchRange];
-    
-    //NSLog(@"UUID generated: %@", sample_uuid);
-    
-    XCTAssertEqual([matches count], (NSUInteger)1,
-                   @"UUID generated doesn't match the type 4 UUID RFC");
-}
-
 // This test is causing xctool problems for some unknown reason
 //
 //- (void)testGetAppleIdfa
