126f7d001e16e57bfe97a13fd10efe98947d0325
Jonathan Almeida <jonathan@snowplowanalytics.com>
2014-05-29 22:51:11 -0400

    Empty TestTracker

---
+++ SnowplowTests/TestTracker.m
@@ -0,0 +1,32 @@
+//
+//  TestTracker.m
+//  Snowplow
+//
+//  Created by Jonathan Almeida on 2014-05-29.
+//  Copyright (c) 2014 Snowplow Analytics. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestTracker : XCTestCase
+
+@end
+
+@implementation TestTracker
+
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+- (void)testExample
+{
+
+}
+
+@end
