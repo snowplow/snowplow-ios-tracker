5872761ce55c7dfa246e684608603cbeb32270cb
Jonathan Almeida <jonathan@snowplowanalytics.com>
2014-07-13 03:46:00 -0400

    Docs for SnowplowEventStore

--- Snowplow/SnowplowEventStore.h
+++ Snowplow/SnowplowEventStore.h
@@ -27,33 +27,78 @@
 
 @property (retain) NSString * appId;
 
+/**
+ *  Basic initializer that creates a database event table (if one does not exist) and then closes the connection.
+ *  @return A SnowplowEventStore object.
+ */
 - (id) init;
 
+/**
+ *  Creates an event table (if one does not exist) within the application data directory using NSLibraryDirectory. This is mainly for internal use.
+ *  @return A bool if the SQL statement is executed without errors. (May return YES if executed even though a table already exists).
+ */
 - (BOOL) createTable;
 
 /**
- * Inserts events into the sqlite table for the app identified with it's bundleId (appId).
- * @param payload A SnowplowPayload instance to be inserted into the database.
- * @return If the insert was successful, we return the rowId of the inserted entry, otherwise -1. We explicitly do this in the case of an error, sqlite would return the previous successful insert leading to incorrect data removals.
+ *  Inserts events into the sqlite table for the app identified with it's bundleId (appId).
+ *  @param payload A SnowplowPayload instance to be inserted into the database.
+ *  @return If the insert was successful, we return the rowId of the inserted entry, otherwise -1. We explicitly do this in the case of an error, sqlite would return the previous successful insert leading to incorrect data removals.
  */
 - (long long int) insertEvent:(SnowplowPayload *)payload;
 
+/**
+ *  Inserts a dictionary into to events table. Mainly used in insertEvent: for SnowplowPayload events.
+ *  @param dict An NSDictionary to be inserted into the database.
+ *  @return If the insert was successful, we return the rowId of the inserted entry, otherwise -1. We explicitly do this in the case of an error, sqlite would return the previous successful insert leading to incorrect data removals.
+ */
 - (long long int) insertDicitionaryData:(NSDictionary *)dict;
 
+/**
+ *  Removes an event from the table with the supplied id.
+ *  @param id_ Unique ID of the row in the events table to be deleted.
+ *  @return Returns the status of the SQL query sent.
+ */
 - (BOOL) removeEventWithId:(long long int)id_;
 
+/**
+ *  Returns the table contents into NSLog. USED ONLY FOR TESTING.
+ */
 - (void) getTable;
 
+/**
+ *  Finds the row in the event table with the supplied ID.
+ *  @param id_ Unique ID of the row in the events table to be returned.
+ *  @return A dictionary containing data with keys: 'ID', 'eventData', and 'dateCreated'.
+ */
 - (NSDictionary *) getEventWithId:(long long int)id_;
 
+/**
+ *  Removes ALL events in the database. USE WITH CARE!
+ */
 - (void) removeAllEvents;
 
+/**
+ *  Number of events in the database.
+ *  @return An integer of the number of events currently in the database.
+ */
 - (NSUInteger) count;
 
+/**
+ *  Returns all the events in an array of dictionaries.
+ *  @return An array with each dictionary element containing key-value pairs of 'date', 'data', 'ID'.
+ */
 - (NSArray *) getAllEvents;
 
-- (NSArray *) getAllPendingEvents;
+/**
+ *  Returns all event data of pending data.
+ *  @return An array of event data with pending set as 1.
+ */
+- (NSArray *) getAllPendingEvents __attribute__((deprecated));
 
+/**
+ *  The row ID of the last insert made.
+ *  @return The row ID of the last insert made.
+ */
 - (long long int) getLastInsertedRowId;
 
 @end
