cb068a2af685f0ba2fb1f0d731c688d906b7693b
Jonathan Almeida <jonathan@snowplowanalytics.com>
2014-08-06 09:33:20 -0400

    Fixed failing test

--- SnowplowTests/TestTracker.m
+++ SnowplowTests/TestTracker.m
@@ -44,7 +44,7 @@
 - (void)testExample
 {
     SnowplowRequest *collector = [[SnowplowRequest alloc] initWithURLRequest:[NSURL URLWithString:@"http://segfault.ngrok.com"] httpMethod:@"POST" bufferOption:SnowplowBufferInstant];
-    SnowplowTracker *tracker = [[SnowplowTracker alloc] initUsingCollector:collector appId:@"foo" base64Encoded:false namespace:@"myname"];
+    SnowplowTracker *tracker = [[SnowplowTracker alloc] initWithCollector:collector appId:@"foo" base64Encoded:false namespace:@"myname"];
     
     NSDictionary *context = [NSDictionary dictionaryWithObjectsAndKeys:
                              @"foo", @"bar", nil];
