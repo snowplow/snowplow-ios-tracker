0a920fd8493d4a9aa4b14a69820a9b60ef655150
Alex Dean <alex@snowplowanalytics.com>
2014-12-18 15:54:01 +0000

    Updated CHANGELOG and bumped version everywhere

--- Snowplow/SnowplowTracker.m
+++ Snowplow/SnowplowTracker.m
@@ -35,7 +35,7 @@
 NSString * const kSnowplowVendor        = @"com.snowplowanalytics.snowplow";
 NSString * const kIglu                  = @"iglu:";
 Boolean    const kDefaultEncodeBase64   = true;
-NSString * const kVersion               = @"ios-0.2.1";
+NSString * const kVersion               = @"ios-0.2.2";
 
 @synthesize collector;
 @synthesize appId;
