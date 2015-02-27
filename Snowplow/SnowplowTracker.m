e3b0890a25fab10245fb8b79f0130fb6bafcb27a
Alex Dean <alex@snowplowanalytics.com>
2015-02-27 10:32:35 +0000

    Bumped version to 0.3.2

--- Snowplow/SnowplowTracker.m
+++ Snowplow/SnowplowTracker.m
@@ -37,9 +37,9 @@ NSString * const kIglu                  = @"iglu:";
 Boolean    const kDefaultEncodeBase64   = true;
 
 #if TARGET_OS_IPHONE
-NSString * const kVersion               = @"ios-0.3.1";
+NSString * const kVersion               = @"ios-0.3.2";
 #else
-NSString * const kVersion               = @"osx-0.3.1";
+NSString * const kVersion               = @"osx-0.3.2";
 #endif
 
 @synthesize collector;
