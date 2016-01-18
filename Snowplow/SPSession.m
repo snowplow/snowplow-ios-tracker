7540cb09d6e6e1e2afeddb8d4ddad018d92dc912
Josh <joshua.a.beemster@gmail.com>
2016-01-18 12:39:20 +0100

    ClientSession needs to send null for the first previousSessionId (fixes #257)

--- Snowplow/SPSession.m
+++ Snowplow/SPSession.m
@@ -63,11 +63,10 @@ NSString * const kSessionSavePath = @"session.dict";
         NSDictionary * maybeSessionDict = [self getSessionFromFile];
         if (maybeSessionDict == nil) {
             _userId = [SPUtilities getEventId];
-            _currentSessionId = @"";
+            _currentSessionId = nil;
         } else {
             _userId = [maybeSessionDict valueForKey:kSPSessionUserId];
             _currentSessionId = [maybeSessionDict valueForKey:kSPSessionId];
-            _previousSessionId = [maybeSessionDict valueForKey:kSPSessionPreviousId];
             _sessionIndex = [[maybeSessionDict valueForKey:kSPSessionIndex] intValue];
         }
         
@@ -163,7 +162,9 @@ NSString * const kSessionSavePath = @"session.dict";
     BOOL result = NO;
     if ([paths count] > 0) {
         NSString * savePath = [[paths lastObject] stringByAppendingPathComponent:kSessionSavePath];
-        NSDictionary * sessionDict = _sessionDict;
+        NSMutableDictionary * sessionDict = [NSMutableDictionary dictionaryWithDictionary:_sessionDict];
+        [sessionDict removeObjectForKey:kSPSessionPreviousId];
+        [sessionDict removeObjectForKey:kSPSessionStorage];
         result = [sessionDict writeToFile:savePath atomically:YES];
     }
     return result;
@@ -214,7 +215,7 @@ NSString * const kSessionSavePath = @"session.dict";
     NSMutableDictionary * newSessionDict = [[NSMutableDictionary alloc] init];
     [newSessionDict setObject:_userId forKey:kSPSessionUserId];
     [newSessionDict setObject:_currentSessionId forKey:kSPSessionId];
-    [newSessionDict setObject:_previousSessionId forKey:kSPSessionPreviousId];
+    [newSessionDict setObject:(_previousSessionId != nil ? _previousSessionId : [NSNull null]) forKey:kSPSessionPreviousId];
     [newSessionDict setObject:[NSNumber numberWithInt:(int)_sessionIndex] forKey:kSPSessionIndex];
     [newSessionDict setObject:_sessionStorage forKey:kSPSessionStorage];
     _sessionDict = newSessionDict;
