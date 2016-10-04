900ec7174fbf96808ccb49d50fadbe19c8812cd0
Josh <joshua.a.beemster@gmail.com>
2016-10-04 18:33:48 +0200

    Bumped VERSION

--- Snowplow/Snowplow.m
+++ Snowplow/Snowplow.m
@@ -27,11 +27,11 @@
 // --- Version
 
 #if SNOWPLOW_TARGET_IOS
-NSString * const kSPVersion               = @"ios-0.6.1";
+NSString * const kSPVersion               = @"ios-0.6.2";
 #elif SNOWPLOW_TARGET_TV
-NSString * const kSPVersion               = @"tvos-0.6.1";
+NSString * const kSPVersion               = @"tvos-0.6.2";
 #else
-NSString * const kSPVersion               = @"osx-0.6.1";
+NSString * const kSPVersion               = @"osx-0.6.2";
 #endif
 
 // --- Emitter
