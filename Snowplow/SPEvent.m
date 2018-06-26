768873d99bd1d7b5e4add548da46be929766afce
Stewart Gleadow <sgleadow@gmail.com>
2018-06-26 21:32:08 +1000

    Resolve potential memory leaks from implicit retain of self (close #353)

--- Snowplow/SPEvent.m
+++ Snowplow/SPEvent.m
@@ -370,23 +370,25 @@
 }
 
 - (NSArray *) getDocuments {
+    __weak __typeof__(self) weakSelf = self;
+    
     // returns the result of appending document passed through {docId, version, name, description} builder arguments to _documents
     NSMutableArray * documents = [[NSMutableArray alloc] init];
-    if (self == nil) {
-        return documents;
-    }
     SPConsentDocument * document = [SPConsentDocument build:^(id<SPConsentDocumentBuilder> builder) {
-        if (self->_documentId != nil) {
-            [builder setDocumentId:self->_documentId];
+        __typeof__(self) strongSelf = weakSelf;
+        if (strongSelf == nil) return;
+
+        if (strongSelf->_documentId != nil) {
+            [builder setDocumentId:strongSelf->_documentId];
         }
-        if (self->_version != nil) {
-            [builder setVersion:self->_version];
+        if (strongSelf->_version != nil) {
+            [builder setVersion:strongSelf->_version];
         }
-        if ([self->_name length] != 0) {
-            [builder setName:self->_name];
+        if ([strongSelf->_name length] != 0) {
+            [builder setName:strongSelf->_name];
         }
-        if ([self->_description length] != 0) {
-            [builder setDescription:self->_description];
+        if ([strongSelf->_description length] != 0) {
+            [builder setDescription:strongSelf->_description];
         }
     }];
     [documents addObject:[document getPayload]];
@@ -534,19 +536,23 @@
 }
 
 - (NSArray *) getDocuments {
+    __weak __typeof__(self) weakSelf = self;
+    
     // returns the result of appending document passed through {docId, version, name, description} to the documents data member
     NSMutableArray * documents = [[NSMutableArray alloc] init];
     if (self == nil) {
         return documents;
     }
     SPConsentDocument * document = [SPConsentDocument build:^(id<SPConsentDocumentBuilder> builder) {
-        [builder setDocumentId:self->_documentId];
-        [builder setVersion:self->_version];
-        if ([self->_name length] != 0) {
-            [builder setName:self->_name];
+        __typeof__(self) strongSelf = weakSelf;
+        if (strongSelf == nil) return;
+        [builder setDocumentId:strongSelf->_documentId];
+        [builder setVersion:strongSelf->_version];
+        if ([strongSelf->_name length] != 0) {
+            [builder setName:strongSelf->_name];
         }
-        if ([self->_description length] != 0) {
-            [builder setDescription:self->_description];
+        if ([strongSelf->_description length] != 0) {
+            [builder setDescription:strongSelf->_description];
         }
     }];
     [documents addObject:[document getPayload]];
