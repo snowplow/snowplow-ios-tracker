770738266b58caf5b2ec211b6ab825cdbd428bba
Michael Hadam <michael@hadam.us>
2019-05-15 17:47:10 -0400

    Prepared for release

--- Snowplow/Snowplow.m
+++ Snowplow/Snowplow.m
@@ -27,11 +27,11 @@
 // --- Version
 
 #if SNOWPLOW_TARGET_IOS
-NSString * const kSPVersion               = @"ios-1.1.1";
+NSString * const kSPVersion               = @"ios-1.1.2";
 #elif SNOWPLOW_TARGET_TV
-NSString * const kSPVersion               = @"tvos-1.1.1";
+NSString * const kSPVersion               = @"tvos-1.1.2";
 #else
-NSString * const kSPVersion               = @"osx-1.1.1";
+NSString * const kSPVersion               = @"osx-1.1.2";
 #endif
 
 // --- Emitter
