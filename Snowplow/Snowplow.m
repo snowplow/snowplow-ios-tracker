c1b60317aacb33ab7c881874055c2d7f3cf69714
Oguzhan Unlu <oguzhan@snowplowanalytics.com>
2019-10-17 15:07:56 +0300

    Prepared for release

--- Snowplow/Snowplow.m
+++ Snowplow/Snowplow.m
@@ -27,11 +27,11 @@
 // --- Version
 
 #if SNOWPLOW_TARGET_IOS
-NSString * const kSPVersion               = @"ios-1.1.2";
+NSString * const kSPVersion               = @"ios-1.1.3";
 #elif SNOWPLOW_TARGET_TV
-NSString * const kSPVersion               = @"tvos-1.1.2";
+NSString * const kSPVersion               = @"tvos-1.1.3";
 #else
-NSString * const kSPVersion               = @"osx-1.1.2";
+NSString * const kSPVersion               = @"osx-1.1.3";
 #endif
 
 // --- Emitter
