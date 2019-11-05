131be3264819b5c6d93f23c98c1949a005b8f490
Alex Benini <alex.benini@snowplowanalytics.com>
2019-11-05 09:46:53 +0100

    Add SwiftCocoapodsDemo project
    It shares the same swift source code with SwiftCarthageDemo project

---
+++ Examples/SnowplowSwiftCocoapodsDemo/Pods/Target Support Files/SnowplowTracker/SnowplowTracker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SnowplowTracker : NSObject
+@end
+@implementation PodsDummy_SnowplowTracker
+@end
---
+++ Examples/SnowplowSwiftCocoapodsDemo/Pods/Target Support Files/SnowplowTracker/SnowplowTracker-umbrella.h
@@ -0,0 +1,27 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "Snowplow.h"
+#import "SPTracker.h"
+#import "SPEmitter.h"
+#import "SPSubject.h"
+#import "SPPayload.h"
+#import "SPUtilities.h"
+#import "SPRequestCallback.h"
+#import "SPRequestResponse.h"
+#import "SPEvent.h"
+#import "SPSelfDescribingJson.h"
+#import "SPScreenState.h"
+
+FOUNDATION_EXPORT double SnowplowTrackerVersionNumber;
+FOUNDATION_EXPORT const unsigned char SnowplowTrackerVersionString[];
+
