c9021eded5236d7d55bfc13b7032b3623e883d48
AlexBenny <AlexBenny@users.noreply.github.com>
2019-11-08 13:19:06 +0100

    Update objc demo app for 1.1.4 (close #461)
    PR #463:
    
    * Update objc demo app to changes up to 1.1.4
    
    - Update method calls (few methods were changed)
    - Update UI layout (it wasn't usable/tappable)
    - Update Podfile and Pods
    
    * Move objc demo app to the Examples folder
    
    * Fix folder name for FMDB header path
    
    * Fix tracking screenId rather than eventId
    
    * Avoid implicit self reference
    
    * Update project settings with Xcode suggestions
    
    * Remove old objc demo app
    
    * Remove workspace as it's needed with Cocoapods only
    
    * Update travis script
    
    * Update .gitignore
    
    * Fix bad events on objc demo app

--- Snowplow/SPEventStore.m
+++ Snowplow/SPEventStore.m
@@ -24,7 +24,7 @@
 #import "SPEventStore.h"
 #import "SPPayload.h"
 #import "SPUtilities.h"
-#import <FMDB/FMDB.h>
+#import <fmdb/FMDB.h>
 
 @implementation SPEventStore {
     NSString *        _dbPath;
