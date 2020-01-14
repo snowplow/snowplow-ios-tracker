4528a4748a37403f6b747461af95763e1cce0add
Alex Benini <alex.benini@snowplowanalytics.com>
2020-01-14 08:45:26 +0100

    Update travis scripts

--- .travis/test_framework.sh
+++ .travis/test_framework.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
-DEST=$1
-PROJECT=$2
-SCHEME=$3
+PROJECT=$1
+DEST_IOS=$2
+SCHEME_IOS=$3
+DEST_WATCH=$4
+SCHEME_WATCH=$5
 
 printf "\n\n Carthage bootstrap \n"
-carthage bootstrap --platform iOS
+carthage bootstrap
 
-printf "\n\n Test framework \n"
-set -o pipefail && xcodebuild -sdk iphonesimulator -destination "${DEST}" ${PROJECT} ${SCHEME} clean test | xcpretty
+printf "\n\n Test framework on iOS \n"
+set -o pipefail && xcodebuild -destination "${DEST_IOS}" ${PROJECT} ${SCHEME_IOS} clean test | xcpretty
+
+printf "\n\n Build framework on watchOS \n"
+set -o pipefail && xcodebuild -destination "${DEST_WATCH}" ${PROJECT} ${SCHEME_WATCH} clean build | xcpretty
