351a8548a7923fbde7d9bff57370c1ecd928e90b
vauxhall <zero.griffin@gmail.com>
2020-04-07 07:43:24 +0200

    Remove other platforms

--- Package.swift
+++ Package.swift
@@ -5,10 +5,7 @@ import PackageDescription
 let package = Package(
     name: "SnowplowTracker",
     platforms: [
-        .iOS(.v8),
-        .macOS(.v10_10),
-        .tvOS(.v9),
-        .watchOS(.v2)
+        .iOS(.v8)
     ],
     products: [
         .library(
