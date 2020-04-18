de7047dc4f08351e9e1c07d36f2f83ca0fc47332
vauxhall <zero.griffin@gmail.com>
2020-04-18 10:49:21 +0200

    Revert module and bridging header

--- Snowplow/Snowplow-Bridging-Header.h
+++ Snowplow/Snowplow-Bridging-Header.h
@@ -1,38 +1,34 @@
-#if SWIFT_PACKAGE
-    
-#else
-    //
-    //  Use this file to import your target's public headers that you would like to expose to Swift.
-    //
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
 
-    #import "Snowplow.h"
-    #import "SPTracker.h"
-    #import "SPEmitter.h"
-    #import "SPSubject.h"
-    #import "SPPayload.h"
-    #import "SPUtilities.h"
-    #import "SPRequestCallback.h"
-    #import "SPSelfDescribingJson.h"
+#import "Snowplow.h"
+#import "SPTracker.h"
+#import "SPEmitter.h"
+#import "SPSubject.h"
+#import "SPPayload.h"
+#import "SPUtilities.h"
+#import "SPRequestCallback.h"
+#import "SPSelfDescribingJson.h"
 
-    // Events
-    #import "SPEventBase.h"
-    #import "SPPageView.h"
-    #import "SPStructured.h"
-    #import "SPUnstructured.h"
-    #import "SPScreenView.h"
-    #import "SPConsentWithdrawn.h"
-    #import "SPConsentDocument.h"
-    #import "SPConsentGranted.h"
-    #import "SPTiming.h"
-    #import "SPEcommerce.h"
-    #import "SPEcommerceItem.h"
-    #import "SPPushNotification.h"
-    #import "SPForeground.h"
-    #import "SPBackground.h"
-    #import "SNOWError.h"
+// Events
+#import "SPEventBase.h"
+#import "SPPageView.h"
+#import "SPStructured.h"
+#import "SPUnstructured.h"
+#import "SPScreenView.h"
+#import "SPConsentWithdrawn.h"
+#import "SPConsentDocument.h"
+#import "SPConsentGranted.h"
+#import "SPTiming.h"
+#import "SPEcommerce.h"
+#import "SPEcommerceItem.h"
+#import "SPPushNotification.h"
+#import "SPForeground.h"
+#import "SPBackground.h"
+#import "SNOWError.h"
 
-    // Global Contexts
-    #import "SPGlobalContext.h"
-    #import "SPSchemaRuleset.h"
-    #import "SPSchemaRule.h"
-#endif
+// Global Contexts
+#import "SPGlobalContext.h"
+#import "SPSchemaRuleset.h"
+#import "SPSchemaRule.h"
