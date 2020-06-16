a53f067fdc46574c582497494e626b6c705ed508
Alex Benini <alex.benini@snowplowanalytics.com>
2020-06-16 13:46:59 +0200

    Fix travis issue

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -13,8 +13,8 @@ fi
 
 printf "\n\n Setup env variables \n"
 
-BUILD_DEST_IOS="platform=iOS Simulator,OS=13.4,name=iPhone 11 Pro"
-BUILD_DEST_PAIRED="platform=iOS Simulator,OS=13.4,name=iPhone 11 Pro"
+BUILD_DEST_IOS="platform=iOS Simulator,OS=13.4.1,name=iPhone 11 Pro"
+BUILD_DEST_PAIRED="platform=iOS Simulator,OS=13.4.1,name=iPhone 11 Pro"
 BUILD_DEST_WATCH="platform=watchOS Simulator,OS=6.2,name=Apple Watch Series 5 - 44mm"
 
 BUILD_PROJECT_LIB="-project Snowplow.xcodeproj"
