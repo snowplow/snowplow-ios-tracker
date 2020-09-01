d79ce969694972839d9ec299076bd93da76b47ef
Alex Benini <alex.benini@snowplowanalytics.com>
2020-09-01 09:47:05 +0200

    Prepare for release

--- Snowplow/Snowplow.m
+++ Snowplow/Snowplow.m
@@ -27,13 +27,13 @@
 // --- Version
 
 #if SNOWPLOW_TARGET_IOS
-NSString * const kSPVersion               = @"ios-1.4.1";
+NSString * const kSPVersion               = @"ios-1.5.0";
 #elif SNOWPLOW_TARGET_TV
-NSString * const kSPVersion               = @"tvos-1.4.1";
+NSString * const kSPVersion               = @"tvos-1.5.0";
 #elif SNOWPLOW_TARGET_WATCHOS
-NSString * const kSPVersion               = @"watchos-1.4.1";
+NSString * const kSPVersion               = @"watchos-1.5.0";
 #else
-NSString * const kSPVersion               = @"osx-1.4.1";
+NSString * const kSPVersion               = @"osx-1.5.0";
 #endif
 
 // --- Emitter
