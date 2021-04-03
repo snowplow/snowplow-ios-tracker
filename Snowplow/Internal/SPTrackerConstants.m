d0220735a6ee9276a25f3642fa8f8f782a13bef9
Alex Benini <alex.benini@snowplowanalytics.com>
2021-04-03 18:34:07 +0200

    Prepare for 2.0.0-beta.0 release

--- Snowplow/Internal/SPTrackerConstants.m
+++ Snowplow/Internal/SPTrackerConstants.m
@@ -27,13 +27,13 @@
 // --- Version
 
 #if SNOWPLOW_TARGET_IOS
-NSString * const kSPVersion               = @"ios-1.7.0";
+NSString * const kSPVersion               = @"ios-2.0.0-beta.0";
 #elif SNOWPLOW_TARGET_TV
-NSString * const kSPVersion               = @"tvos-1.7.0";
+NSString * const kSPVersion               = @"tvos-2.0.0-beta.0";
 #elif SNOWPLOW_TARGET_WATCHOS
-NSString * const kSPVersion               = @"watchos-1.7.0";
+NSString * const kSPVersion               = @"watchos-2.0.0-beta.0";
 #else
-NSString * const kSPVersion               = @"osx-1.7.0";
+NSString * const kSPVersion               = @"osx-2.0.0-beta.0";
 #endif
 
 // --- Emitter
