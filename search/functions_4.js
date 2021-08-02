4fa08b7941f6288b9aa180e81b625abc885f7fc0
AlexBenny <AlexBenny@users.noreply.github.com>
2021-08-02 12:04:31 +0000

    deploy: 2cf806e834a75ce801c46f0182f23b481655762b

--- search/functions_4.js
+++ search/functions_4.js
@@ -1,11 +1,11 @@
 var searchData=
 [
-  ['debug_3amessage_3a_1499',['debug:message:',['../protocol_s_p_logger_delegate-p.html#a51196afe5683f55e04f461c574651c52',1,'SPLoggerDelegate-p::debug:message:()'],['../interface_s_p_logger.html#a51196afe5683f55e04f461c574651c52',1,'SPLogger::debug:message:()'],['../protocol_s_p_logger_delegate-p.html#a51196afe5683f55e04f461c574651c52',1,'SPLoggerDelegate-p::debug:message:()']]],
-  ['defaulttracker_1500',['defaultTracker',['../interface_s_p_snowplow.html#a66d4188c04dc962de2883ccfcf9fd386',1,'SPSnowplow::defaultTracker()'],['../interface_s_p_snowplow.html#af88c6a4e3eba13d0aed98d7bf438e84c',1,'SPSnowplow::defaultTracker()']]],
-  ['deprecated_5fattribute_1501',['DEPRECATED_ATTRIBUTE',['../interface_s_p_session.html#ae9bde39a296cb7d8fd8a988fd22a5805',1,'SPSession']]],
-  ['description_1502',['description',['../interface_s_p_self_describing_json.html#a0920dc7935061bc57daab91706be5934',1,'SPSelfDescribingJson::description()'],['../interface_s_p_self_describing_json.html#a0920dc7935061bc57daab91706be5934',1,'SPSelfDescribingJson::description()']]],
-  ['deserializedata_3a_1503',['deserializeData:',['../interface_s_p_j_s_o_n_serialization.html#ae58f498b40633c0aa0bb0ead82ea1e52',1,'SPJSONSerialization']]],
-  ['diagnostic_3amessage_3aerrororexception_3a_1504',['diagnostic:message:errorOrException:',['../interface_s_p_logger.html#a3d545afaac963331161b72b43931219c',1,'SPLogger']]],
-  ['disable_1505',['disable',['../protocol_s_p_g_d_p_r_controller-p.html#a8cfbbe53c1cf6e3054736daea3044c0f',1,'SPGDPRController-p::disable()'],['../protocol_s_p_g_d_p_r_controller-p.html#a8cfbbe53c1cf6e3054736daea3044c0f',1,'SPGDPRController-p::disable()']]],
-  ['disablegdprcontext_1506',['disableGdprContext',['../interface_s_p_tracker.html#a53a79fba45cd3d29e99cac2ce5f21234',1,'SPTracker::disableGdprContext()'],['../interface_s_p_tracker.html#a53a79fba45cd3d29e99cac2ce5f21234',1,'SPTracker::disableGdprContext()']]]
+  ['debug_3amessage_3a_1498',['debug:message:',['../protocol_s_p_logger_delegate-p.html#a51196afe5683f55e04f461c574651c52',1,'SPLoggerDelegate-p::debug:message:()'],['../interface_s_p_logger.html#a51196afe5683f55e04f461c574651c52',1,'SPLogger::debug:message:()'],['../protocol_s_p_logger_delegate-p.html#a51196afe5683f55e04f461c574651c52',1,'SPLoggerDelegate-p::debug:message:()']]],
+  ['defaulttracker_1499',['defaultTracker',['../interface_s_p_snowplow.html#a66d4188c04dc962de2883ccfcf9fd386',1,'SPSnowplow::defaultTracker()'],['../interface_s_p_snowplow.html#af88c6a4e3eba13d0aed98d7bf438e84c',1,'SPSnowplow::defaultTracker()']]],
+  ['deprecated_5fattribute_1500',['DEPRECATED_ATTRIBUTE',['../interface_s_p_session.html#ae9bde39a296cb7d8fd8a988fd22a5805',1,'SPSession']]],
+  ['description_1501',['description',['../interface_s_p_self_describing_json.html#a0920dc7935061bc57daab91706be5934',1,'SPSelfDescribingJson::description()'],['../interface_s_p_self_describing_json.html#a0920dc7935061bc57daab91706be5934',1,'SPSelfDescribingJson::description()']]],
+  ['deserializedata_3a_1502',['deserializeData:',['../interface_s_p_j_s_o_n_serialization.html#ae58f498b40633c0aa0bb0ead82ea1e52',1,'SPJSONSerialization']]],
+  ['diagnostic_3amessage_3aerrororexception_3a_1503',['diagnostic:message:errorOrException:',['../interface_s_p_logger.html#a3d545afaac963331161b72b43931219c',1,'SPLogger']]],
+  ['disable_1504',['disable',['../protocol_s_p_g_d_p_r_controller-p.html#a8cfbbe53c1cf6e3054736daea3044c0f',1,'SPGDPRController-p::disable()'],['../protocol_s_p_g_d_p_r_controller-p.html#a8cfbbe53c1cf6e3054736daea3044c0f',1,'SPGDPRController-p::disable()']]],
+  ['disablegdprcontext_1505',['disableGdprContext',['../interface_s_p_tracker.html#a53a79fba45cd3d29e99cac2ce5f21234',1,'SPTracker::disableGdprContext()'],['../interface_s_p_tracker.html#a53a79fba45cd3d29e99cac2ce5f21234',1,'SPTracker::disableGdprContext()']]]
 ];
