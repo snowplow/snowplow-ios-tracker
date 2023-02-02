0a35338cea2c2ee5faede7160101e530211e7fca
Matúš Tomlein <matus.tomlein@gmail.com>
2023-02-02 12:21:29 +0100

    Update year in copyright headers and remove authors from headers in source files

--- Sources/Snowplow/Events/MessageNotification.swift
+++ Sources/Snowplow/Events/MessageNotification.swift
@@ -1,8 +1,4 @@
-//
-// MessageNotification.m
-// Snowplow
-//
-// Copyright (c) 2013-2022 Snowplow Analytics Ltd. All rights reserved.
+// Copyright (c) 2013-2023 Snowplow Analytics Ltd. All rights reserved.
 //
 // This program is licensed to you under the Apache License Version 2.0,
 // and you may not use this file except in compliance with the Apache License
@@ -14,9 +10,6 @@
 // an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 // express or implied. See the Apache License Version 2.0 for the specific
 // language governing permissions and limitations there under.
-//
-// License: Apache License Version 2.0
-//
 
 import Foundation
 
