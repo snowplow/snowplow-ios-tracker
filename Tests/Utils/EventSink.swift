c2c08d504d5268b9b0839f0507e4d8a8337b5eb7
Matúš Tomlein <matus.tomlein@gmail.com>
2023-11-10 09:52:14 +0100

    Fix file header

--- Tests/Utils/EventSink.swift
+++ Tests/Utils/EventSink.swift
@@ -1,9 +1,15 @@
+//  Copyright (c) 2013-2023 Snowplow Analytics Ltd. All rights reserved.
 //
-//  File.swift
-//  
+//  This program is licensed to you under the Apache License Version 2.0,
+//  and you may not use this file except in compliance with the Apache License
+//  Version 2.0. You may obtain a copy of the Apache License Version 2.0 at
+//  http://www.apache.org/licenses/LICENSE-2.0.
 //
-//  Created by Matus Tomlein on 13/10/2023.
-//
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the Apache License Version 2.0 is distributed on
+//  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+//  express or implied. See the Apache License Version 2.0 for the specific
+//  language governing permissions and limitations there under.
 
 import Foundation
 @testable import SnowplowTracker
