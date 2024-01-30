89458ec7549f786157b029b51c1db04c6ff1035f
Matúš Tomlein <matus.tomlein@gmail.com>
2024-01-30 10:03:55 +0100

    Add full stops to docstrings

--- Sources/Snowplow/Configurations/PlatformContextProperty.swift
+++ Sources/Snowplow/Configurations/PlatformContextProperty.swift
@@ -13,41 +13,41 @@
 
 import Foundation
 
-/// Optional properties tracked in the platform context entity
+/// Optional properties tracked in the platform context entity.
 public enum PlatformContextProperty: Int {
-    /// The carrier of the SIM inserted in the device
+    /// The carrier of the SIM inserted in the device.
     case carrier
-    /// Type of network the device is connected to
+    /// Type of network the device is connected to.
     case networkType
-    /// Radio access technology that the device is using
+    /// Radio access technology that the device is using.
     case networkTechnology
-    /// Advertising identifier on iOS
+    /// Advertising identifier on iOS.
     case appleIdfa
-    /// UUID identifier for vendors on iOS
+    /// UUID identifier for vendors on iOS.
     case appleIdfv
-    /// Total physical system memory in bytes
+    /// Total physical system memory in bytes.
     case physicalMemory
-    /// Amount of memory in bytes available to the current app
+    /// Amount of memory in bytes available to the current app.
     /// The property is not tracked in the current version of the tracker due to the tracker not being able to access the API, see the issue here: https://github.com/snowplow/snowplow-ios-tracker/issues/772
     case appAvailableMemory
-    /// Remaining battery level as an integer percentage of total battery capacity
+    /// Remaining battery level as an integer percentage of total battery capacity.
     case batteryLevel
-    /// Battery state for the device
+    /// Battery state for the device.
     case batteryState
-    /// A Boolean indicating whether Low Power Mode is enabled
+    /// A Boolean indicating whether Low Power Mode is enabled.
     case lowPowerMode
-    /// Bytes of storage remaining
+    /// Bytes of storage remaining.
     /// Note: This is not automatically assigned by the tracker as it may be considered as fingerprinting. You can assign it using the PlatformContextRetriever.
     case availableStorage
     /// Total size of storage in bytes
     /// Note: This is not automatically assigned by the tracker as it may be considered as fingerprinting. You can assign it using the PlatformContextRetriever.
     case totalStorage
-    /// A Boolean indicating whether the device orientation is portrait (either upright or upside down)
+    /// A Boolean indicating whether the device orientation is portrait (either upright or upside down).
     case isPortrait
-    /// Screen resolution in pixels. Arrives in the form of WIDTHxHEIGHT (e.g., 1200x900). Doesn't change when device orientation changes
+    /// Screen resolution in pixels. Arrives in the form of WIDTHxHEIGHT (e.g., 1200x900). Doesn't change when device orientation changes.
     case resolution
-    /// Scale factor used to convert logical coordinates to device coordinates of the screen (uses UIScreen.scale on iOS)
+    /// Scale factor used to convert logical coordinates to device coordinates of the screen (uses UIScreen.scale on iOS).
     case scale
-    /// System language currently used on the device (ISO 639)
+    /// System language currently used on the device (ISO 639).
     case language
 }
