9b11e7b0df2df7694b42149a8ebb6108dbcba7a8
Miranda Wilson <miranda@snowplow.io>
2024-01-30 18:02:36 +0000

    Update copyright notices (close #868)
    * Update copyright notices
    
    * Update more files

--- Sources/Snowplow/Media/Events/MediaPlaybackRateChangeEvent.swift
+++ Sources/Snowplow/Media/Events/MediaPlaybackRateChangeEvent.swift
@@ -1,4 +1,4 @@
-//  Copyright (c) 2013-2023 Snowplow Analytics Ltd. All rights reserved.
+//  Copyright (c) 2013-present Snowplow Analytics Ltd. All rights reserved.
 //
 //  This program is licensed to you under the Apache License Version 2.0,
 //  and you may not use this file except in compliance with the Apache License
